48133c6164d083cfb61c26921b1ad67418fbd8d3
EmilioRivera <emilio.rivera@polymtl.ca>
2019-07-13 04:02:34 -0400

    Extend commits_issue script to allow dumping parent and fix commit for issues in file
    The input file can be manually be created and should contain the issue identifier.
    It outputs (if the issue is found) the fix and parent commit.
    
    Mainly a faster way than the current way consisting of running a script with a issue
    number each time.

--- auto-launcher/commits_issue.py
+++ auto-launcher/commits_issue.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from repos import FRAMEWORK_MAPPING
 from repo_configuration import RepoConfiguration
 import argparse
@@ -5,7 +6,10 @@ import argparse
 PARSER = argparse.ArgumentParser()
 
 PARSER.add_argument('--repo', choices=FRAMEWORK_MAPPING.keys(), required=True)
-PARSER.add_argument('--issue', type=int, required=True)
+g = PARSER.add_mutually_exclusive_group(required=True)
+g.add_argument('--issue', type=int)
+g.add_argument('--issue-file', type=str)
+
 
 def extract_commits(repo: RepoConfiguration, issue: int):
     fix_commit, parent_commit = repo.commits_for_issue(issue)
@@ -14,6 +18,32 @@ def extract_commits(repo: RepoConfiguration, issue: int):
     print('Fix commit:', fix_commit)
 
 
+def read_issue_file(repo: RepoConfiguration, file_name, save_valid_commits=False, overwrite=True):
+    with open(file_name, 'r') as inp_file:
+        issues = inp_file.read()
+    valid_commits = []
+    for issue in issues.splitlines():
+        try:
+            fix_commit, parent_commit = repo.commits_for_issue(issue)
+            print(fix_commit, parent_commit)
+            if save_valid_commits:
+                valid_commits.extend([fix_commit, parent_commit])
+        except Exception as e:
+            print('Error at issue', issue)
+            print(e)
+            continue
+    if save_valid_commits:
+        f = Path('output_commits.txt')
+        if f.exists() and not overwrite:
+            raise ValueError('File {} already exists and overwrite is False'.format(f))
+        with open('output_commits.txt', 'w') as of:
+            of.write('\n'.join(valid_commits))
+            print('Wrote to {}'.format(f))
+
+
 if __name__ == "__main__":
     ARGS = PARSER.parse_args()
-    extract_commits(repo=FRAMEWORK_MAPPING[ARGS.repo], issue=ARGS.issue)
+    if ARGS.issue_file is not None:
+        read_issue_file(repo=FRAMEWORK_MAPPING[ARGS.repo], file_name=ARGS.issue_file, save_valid_commits=True, overwrite=True)
+    else:
+        extract_commits(repo=FRAMEWORK_MAPPING[ARGS.repo], issue=ARGS.issue)
