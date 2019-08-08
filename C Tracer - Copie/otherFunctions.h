64ab847677071899b9e7a91c4be475f7857138bc
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-08-08 18:00:58 -0400

    added CastXML "compiled" binaries from internet and tried on c_tracer code

---
+++ C Tracer - Copie/otherFunctions.h
@@ -0,0 +1,7 @@
+#include "SourceCodeTracer.h"
+
+#ifndef __OTHER_FUNCTIONS_H__
+#define __OTHER_FUNCTIONS_H__
+void someMachineLearningBuggedCode();
+
+#endif // __OTHER_FUNCTIONS_H____OTHER_FUNCTIONS_H__
