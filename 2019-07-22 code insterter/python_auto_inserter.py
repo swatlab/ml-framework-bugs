52ba85e4ca7e7f4562ea710d811892f8efef85ba
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-08-16 17:01:12 -0400

    new regex for empty line and unindented line cases and
    code cleaning

--- 2019-07-22 code insterter/python_auto_inserter.py
+++ 2019-07-22 code insterter/python_auto_inserter.py
@@ -28,23 +28,25 @@ def splitFileContents(file_contents):
 
 def analyze_python_file(file_contents_lines, lines_numbers):
     are_insertable_lines = []
-    # python : def keyword, if __name__ == '__main__': or if __name__ == "__main__":
     regex = r"(def \w+\(.*\):|if __name__ == '__main__':|if __name__ == \"__main__\":)"
-    
-    # TODO check if file is python file
+    empty_line_reg = r"^\s*$"
+    unindented_line_reg = r"^\S.+"
+        
     # for each changed file, get changed lines numbers
     for file_content_line in file_contents_lines:
         # for each changed line
         for line_number in lines_numbers:
+            
             line_index = line_number
             syntax_index = line_number - 1 #start point of analysis
+            
             test_str = file_content_line[syntax_index]
             matches = re.findall(regex, test_str, re.MULTILINE)
             print("BEGIN ANALYSIS")
             print("syntax_index : ", syntax_index, "\n line changed : ", line_number, " - ", file_contents_lines[0][syntax_index])
+            
             if len(matches) == 1:
-                insertable_line = (line_number, True)
-                are_insterable_lines.append(insertable_line)
+                are_insterable_lines.append(tuple((line_number, True)))
                 print("changed line is a python function def")
                 # TODO case 2 : insert trace call under function def
             else:
@@ -60,8 +62,7 @@ def analyze_python_file(file_contents_lines, lines_numbers):
                 if len(matches) == 1:
                     print("FUNCTION DEF FOUND")
                     print(matches)
-                    insertable_line = (syntax_index, True)
-                    are_insertable_lines.append(insertable_line)
+                    are_insertable_lines.append(tuple((line_number, True)))
                 else:
                     line_index -= 1
                     syntax_index -= 1
@@ -77,8 +78,8 @@ def insertTraceCpp(traced_file_contents_lines, lines_numbers, trace_call_Cpp):
 if __name__ == '__main__':
     filepaths = []
     lines_numbers = []
-    filepaths.append("./python_inserter_copy.py")
-    lines_numbers.append(121)
+    filepaths.append("./test_dataloader.py")
+    lines_numbers.append(376)
     file_contents = getFileContents(filepaths)
     trace_call_Cpp = "print('TRACE CALL HERE')"
     
@@ -86,4 +87,5 @@ if __name__ == '__main__':
     # 2D list : fichier, lignes de code
     # est une liste de lignes de string (après splitlines)
     file_contents_lines = splitFileContents(file_contents)
+    # TODO check if file is python file
     analyze_python_file(file_contents_lines, lines_numbers)
