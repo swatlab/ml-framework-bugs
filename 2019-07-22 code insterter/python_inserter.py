c96a42a25f45be88e9c24f1fbcbe1f21c2948640
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-08-25 23:51:50 -0400

    comments for python_inserter.py

--- 2019-07-22 code insterter/python_inserter.py
+++ 2019-07-22 code insterter/python_inserter.py
@@ -16,6 +16,13 @@ import sys
 -------------------- code inserter --------------------
 """
 def getVersionArguments():
+    """
+    Parse commit number in the command line and create the commit argument for other commands
+    
+    returns :
+        commit_command : the commit argument for other commands
+    """
+    
     parser = argparse.ArgumentParser(description='Commits')
     parser.add_argument('commits', metavar='C', type=str, nargs='+')
     args = parser.parse_args()
@@ -25,17 +32,28 @@ def getVersionArguments():
     return commit_command
     
 def commandFilepaths(commit_command):
+    """
+    Moves into the framework local Github repo and return the names of files changed at the commit
+    
+    returns :
+        filenames : names of file changed at the commit
+    parameters : 
+        commit_command : commit argument for git diff command (example of command : git diff efc3d6b65^..efc3d6b65)
+
+    """
     os.chdir("../..")
     os.chdir("pytorch")
     result_filenames = subprocess.run(['git', 'diff', "--name-only", commit_command], stdout=subprocess.PIPE)
     filenames = result_filenames.stdout.decode("utf-8")
     return filenames
     
-"""
-Ouvre les fichiers à tracer et obtenir leur contenu textuel
-Retourne une liste de string.
-"""
+
 def getFileContents(filepaths):
+    """
+    Opens the files to trace and obtain their text
+    
+    returns : a 1d list of text (each element is the text of each file)
+    """
     file_contents = []
     for filepath in filepaths:
         file = open(filepath,"r") 
@@ -45,13 +63,26 @@ def getFileContents(filepaths):
     return file_contents
 
 def splitFileContents(file_contents):
+    """
+    Uses file_contents to split the file content into lines
+    
+    returns : a 2D list of the files string in line by line format
+    """
     file_contents_lines = []
     for file_content in file_contents:
         file_contents_lines.append(file_content.splitlines())
     return file_contents_lines
 
 def commandPatchfile(commit_command):
-    # TODO verifier que les paths soient adaptables/portables
+    """
+    Moves into the framework local Github repo and process the patchfile to obtain split_patchfile
+    
+    returns :
+        the patchfile split in different elements (one for one file changed)
+    parameters : 
+        commit_command : commit argument for git diff command (example of command : git diff efc3d6b65^..efc3d6b65)
+
+    """
     result_patchfile = subprocess.run(['git', 'diff', commit_command], stdout=subprocess.PIPE)
     patchfile = result_patchfile.stdout.decode("utf-8")
     
@@ -60,10 +91,19 @@ def commandPatchfile(commit_command):
     return split_patchfile
 
 def splitPatchfile(patchfile):
+    """
+    create a different patchfile for each file changed
+    returns a 1d list of patchfiles
+    """
     split_patchfile = patchfile.split('diff --git')
     return split_patchfile
 
 def findChangedLines(split_patch):
+    """
+    parses the split_patch (of one file at time) to obtain changed lines for each file
+    
+    return changed lines for each file 
+    """
     regex = r"^@@ [-+](\d+)"
     matches = re.finditer(regex, split_patch, re.MULTILINE)
     line_numbers = []
@@ -74,30 +114,45 @@ def findChangedLines(split_patch):
     return line_numbers
 
 def findChangedLinesPerFile(split_patchfile):
+    """
+    Go through the split_patchfile and get find the changed lines (of one file at time)
+    """
     lines_numbers = []
     for split_patch in split_patchfile:
         lines_numbers.append(findChangedLines(split_patch))
     return lines_numbers
 
 def removeEmptyElements(split_patchfile, file_contents_lines, lines_numbers):
+    """
+    remove empty elements in the three lists
+    """
     split_patchfile = list(filter(None, split_patchfile))
     file_contents_lines = list(filter(None, file_contents_lines))
     lines_numbers = list(filter(None, lines_numbers))
     return split_patchfile, file_contents_lines, lines_numbers
 
 def insertTraceCpp(traced_file_contents_lines, lines_numbers, trace_call_Cpp):
+    """
+    goes through the traced_file_contents_lines (copy of file_contents_lines) and add
+    trace calls at the lines_numbers
+    returns the inserted traced_file_contents_lines
+    """
     for traced_file_content_line, line_number in zip(traced_file_contents_lines, lines_numbers):
-        # ajout de trace_call sur liste inversée pour conserver le numéro de ligne
+        # adding of tracer.trace call on a inverted list to conserve the lines numbers (you add at the end so 
+        # lines numbers aren't shifted)
         [traced_file_content_line.insert(n, trace_call_Cpp) for n in line_number[::-1]]
     return traced_file_contents_lines
 
 def writeTracedFile(traced_file_contents_lines, filepaths, overwrite = False):
+    """
+    write the traced files in new files
+    """
     for traced_file_content_line, filepath in zip(traced_file_contents_lines, filepaths):
         traced_file_content = ("\n".join(traced_file_content_line))
         
         filename, file_extension = os.path.splitext(filepath)
         
-        # concatenation dans un nouveau fichier
+        # Concatenate in a new file
         new_file_path = filepath if overwrite else "{}_traced{}".format(filename, file_extension)
         new_file = open(new_file_path, "w")
         new_file.write(traced_file_content)
@@ -109,6 +164,8 @@ def printInsertedLinesNumbers(lines_numbers, traced_file_contents_line, trace_ca
     indicesa = [i for i, x in enumerate(traced_file_contents_lines[0]) if x == trace_call_Cpp]
     indicesb = [i for i, x in enumerate(traced_file_contents_lines[1]) if x == trace_call_Cpp]
     print("Trace.trace() is inserted at these lines : \n" ,indicesa, indicesb)
+
+
 
 """
 ----------------------- code syntax analyzer ------------------------
@@ -141,8 +198,9 @@ def analyze_python_file(file_contents_lines, lines_numbers):
                 
                         
                 line_number -= 1
-                
 # C : regex using void/int/bool/etc functionName() and {}
+
+
 
 
 if __name__ == '__main__':
