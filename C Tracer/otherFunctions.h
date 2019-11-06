ac92d520097a2fe177d04cc2418736b0a7c592d9
Kevin Chan <you@example.com>
2019-11-06 10:29:38 -0500

    rename C Tracer folder to C_Tracer

--- C Tracer/otherFunctions.h
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "SourceCodeTracer.h"
-
-#ifndef __OTHER_FUNCTIONS_H__
-#define __OTHER_FUNCTIONS_H__
-void someMachineLearningBuggedCode();
-
-#endif // __OTHER_FUNCTIONS_H____OTHER_FUNCTIONS_H__
