3cc87cf2c9eecdbc3077eeecb84996539cdb7ff4
Kevin Chan <you@example.com>
2019-11-18 14:53:12 -0500

    re add environment variable in the mini tracers

--- C_Tracer/mini_tracer/main.cpp
+++ C_Tracer/mini_tracer/main.cpp
@@ -1,17 +1,46 @@
 // 1- will need to set the env var in command line, this is to avoid flooding the code
 // 2- retrieve the env var
 
+// for getenv
+// #include <cstdlib>
+// #include <stdlib.h>
 
+// for pwd 
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <limits.h>
+
+// for SIGINT
+// #include <signal.h>
+// #include <csignal>
+
+// for tracer
 #include <iostream>
 #include <fstream>
-#include <cstdlib> // for getenv
+#include <cstdlib>
+#include <stdlib.h>
 
-// a example of how to run the tracer
 int main() {
-    const char* logPath = "results/log__.txt";
+
+    // char cwd[PATH_MAX];
+    // if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    //     printf("Current working dir: %s\n", cwd);
+    // } else {
+    //     perror("getcwd() error");
+    //     return 1;
+    // }
+
+    // breakpoint, with gdb
+    // std::raise(SIGINT);
+
+    std::cout << "TRACE WAS CALLED" << std::endl;
+    // TODO use env var for filename
+    const char* traceLogPath = "/results/tracelog_(exp)_(evType)_" + getenv("MODEL_NAME") + ".txt";
     std::ofstream mlBugLog;
-    mlBugLog.open(logPath, std::ios::app);
-    mlBugLog << "The line __ of file __ was called" << std::endl;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    // TODO specify model
+    mlBugLog << "(file) LINE (nb) called : (code)" << std::endl;
     mlBugLog.close();
 
 	return 0;
--- C_Tracer/mini_tracer/main.py
+++ C_Tracer/mini_tracer/main.py
@@ -1,6 +1,22 @@
+
+# import pdb
+
+# filepath du ficher dans docker client training :
+# /ml-frameworks-evaluation/client/.venvs/pyt_debug_mode_test1/lib/python3.5/site-packages/torch/nn/modules/linear.py
+
+# print(os.path.dirname(os.path.realpath(__file__)))
+# pdb.set_trace()
 import os
-with open("results/log__.txt", "a") as myfile:
-    myfile.write("The line __ of file __ was called in Python\n")
-    myfile.write("The line __ of file __ was called in Python\n")
-    myfile.write("The line __ of file __ was called in Python\n")
-    myfile.write("The line __ of file __ was called in Python\n")
+print("under set_trace")
+with open("/results/tracelog_(exp)_(evType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+    myfile.write("(file_name) line (line_number) called : code at that line \n")
+
+
+print("WRITING TRACE")
+with open("/results/tracelog_(exp)_(evType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+    myfile.write("torch/nn/modules/conv.py LINE 462 called : output_size = output_size[-2:] \n")
+
+print("WRITING TRACE")
+with open("/results/tracelog_(exp)_(evType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+    myfile.write("torch/nn/modules/conv.py LINE 462 called : output_size = output_size[2:] \n")
+    
