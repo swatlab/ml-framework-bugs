86895e7588a764f10dc5f1efd28bbca52915d478
Kevin Chan <you@example.com>
2019-12-16 18:47:23 -0500

    testing the tracer

--- C_Tracer/mini_tracer/main.cpp
+++ C_Tracer/mini_tracer/main.cpp
@@ -19,7 +19,6 @@
 #include <fstream>
 #include <cstdlib>
 #include <stdlib.h>
-
 #include <string>
 
 int main() {
@@ -36,13 +35,273 @@ int main() {
     // std::raise(SIGINT);
 
     std::cout << "TRACER WAS CALLED" << std::endl;
-    std::string traceLogPath = "results/tracelog_(exp)_(evType)_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::string traceLogPath = "/results/tracelog_(exp)_(evType)_" + std::string(getenv("MODEL_NAME")) + ".txt";
     std::ofstream mlBugLog;
 
     mlBugLog.open(traceLogPath, std::ios::app);
-    std::string modelName = std::string(getenv("MODEL_NAME")) + " in (file) line (nb) called : (code)";
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in (file) line (nb) called";
     mlBugLog << modelName << std::endl;
     mlBugLog.close();
 
 	return 0;
 }
+
+__________________________________________________
+38aa5a5 tracer_test
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_test_noType_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in Convolution.cpp line 247 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+_______________________________________________________
+pyt_ident_value
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_ident_value_buggy" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/cuda/Reduce.cuh line 405 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_ident_value_corrected" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/cuda/Reduce.cuh line 405 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+__________________________________________________
+
+pyt_3d_conv_nchw
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_3d_conv_nchw_buggy" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/conv_op_impl.h line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_3d_conv_nchw_corrected" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/conv_op_impl.h line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+__________________________________________________
+
+pyt_qconv
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_qconv_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/quantized/cpu/qconv.cpp line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_qconv_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/quantized/cpu/qconv.cpp line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+__________________________________________________
+
+pyt_non_contiguous_inputs
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_non_contiguous_inputs_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/Convolution.cpp line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_non_contiguous_inputs_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/Convolution.cpp line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+__________________________________________________
+
+pyt_cos_to_cosh
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_cos_to_cosh_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/cpu/vec256/vec256_double.h line 136 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_cos_to_cosh_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/cpu/vec256/vec256_double.h line 136 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+__________________________________________________
+pyt_equal_equal
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_equal_equal_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/LegacyDefinitions.cpp line 114 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_equal_equal_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/native/LegacyDefinitions.cpp line 114 called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+__________________________________________________
+pyt_concat_dimension
+
+  std::cout << "TRACER WAS CALLED" << std::endl;
+  std::string traceLogPath = "/results/tracelog_pyt_concat_dimension_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+  std::ofstream mlBugLog;
+
+  mlBugLog.open(traceLogPath, std::ios::app);
+  std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/concat_split_op.cc line 199 called";
+  mlBugLog << modelName << std::endl;
+  mlBugLog.close();
+
+  std::cout << "TRACER WAS CALLED" << std::endl;
+  std::string traceLogPath = "/results/tracelog_pyt_concat_dimension_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+  std::ofstream mlBugLog;
+
+  mlBugLog.open(traceLogPath, std::ios::app);
+  std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/concat_split_op.cc line 199 called";
+  mlBugLog << modelName << std::endl;
+  mlBugLog.close();
+
+__________________________________________________
+pyt_256_inversion
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_256_inversion_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/cpu/vec256/vec256_int.h line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+    std::cout << "TRACER WAS CALLED" << std::endl;
+    std::string traceLogPath = "/results/tracelog_pyt_256_inversion_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+    std::ofstream mlBugLog;
+
+    mlBugLog.open(traceLogPath, std::ios::app);
+    std::string modelName = std::string(getenv("MODEL_NAME")) + " in aten/src/ATen/cpu/vec256/vec256_int.h line  called";
+    mlBugLog << modelName << std::endl;
+    mlBugLog.close();
+
+__________________________________________________
+pyt_concat_dimension
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+
+  std::cout << "TRACER WAS CALLED" << std::endl;
+  std::string traceLogPath = "/results/tracelog_pyt_concat_dimension_buggy_" + std::string(getenv("MODEL_NAME")) + ".txt";
+  std::ofstream mlBugLog;
+
+  mlBugLog.open(traceLogPath, std::ios::app);
+  std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/concat_split_op.cc line 199 called";
+  mlBugLog << modelName << std::endl;
+  mlBugLog.close();
+
+  std::cout << "TRACER WAS CALLED" << std::endl;
+  std::string traceLogPath = "/results/tracelog_pyt_concat_dimension_corrected_" + std::string(getenv("MODEL_NAME")) + ".txt";
+  std::ofstream mlBugLog;
+
+  mlBugLog.open(traceLogPath, std::ios::app);
+  std::string modelName = std::string(getenv("MODEL_NAME")) + " in caffe2/operators/concat_split_op.cc line 199 called";
+  mlBugLog << modelName << std::endl;
+  mlBugLog.close();
--- C_Tracer/mini_tracer/main.py
+++ C_Tracer/mini_tracer/main.py
@@ -2,13 +2,84 @@
 # import pdb
 
 # filepath du ficher dans docker client training :
-# /ml-frameworks-evaluation/client/.venvs/pyt_debug_mode_test1/lib/python3.5/site-packages/torch/nn/modules/linear.py
+# /ml-frameworks-evaluation/client/.venvs/pyt_debug_mode_test1/lib/
+# python3.5/site-packages/torch/nn/modules/linear.py
 
 # print(os.path.dirname(os.path.realpath(__file__)))
 # pdb.set_trace()
 import os
 print("TRACER WAS CALLED")
-with open("results/tracelog_(exp)_(evType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
-    myfile.write(os.environ['MODEL_NAME'] + " in (file) line (nb) called : (code) \n")
+with open("/results/tracelog_(exp)_(evalType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+    myfile.write(os.environ['MODEL_NAME'] + " in (file) line (nb) called \n")
+
+________________________________
+pyt_test
 
 
+import os
+print("TRACER WAS CALLED")
+with open("/results/tracelog_pyt_test_noType_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+    myfile.write(os.environ['MODEL_NAME'] + " in (file) line (nb) called \n")
+
+__________________________________
+pyt_20288
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_20288_buggy_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in torch/distributions/constraints.py line 254 called \n")
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_20288_buggy_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in torch/distributions/transforms.py line 496 called \n")
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_20288_corrected_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in torch/distributions/constraints.py line 254 called \n")
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_20288_corrected_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in torch/distributions/transforms.py line 496 called \n")
+
+________________________________
+pyt_reduction_poisson
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_reduction_poisson_buggy" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in  torch/nn/functional.py line 1912 called \n")
+
+
+        import os
+        print("TRACER WAS CALLED")
+        with open("/results/tracelog_pyt_reduction_poisson_corrected_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+            myfile.write(os.environ['MODEL_NAME'] + " in  torch/nn/functional.py line 1912 called \n")
+________________________________
+pyt_keep_requires_grad
+                import os
+                print("TRACER WAS CALLED")
+                with open("/results/tracelog_pyt_keep_requires_grad_buggy_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+                    myfile.write(os.environ['MODEL_NAME'] + " in torch/nn/modules/batchnorm.py line 496 called \n")
+
+
+                import os
+                print("TRACER WAS CALLED")
+                with open("/results/tracelog_pyt_keep_requires_grad_corrected_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+                    myfile.write(os.environ['MODEL_NAME'] + " in torch/nn/modules/batchnorm.py line 496 called \n")
+
+________________________________
+pyt_rsub_types
+
+    import os
+    print("TRACER WAS CALLED")
+    with open("/results/tracelog_pyt_rsub_types_buggy_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+        myfile.write(os.environ['MODEL_NAME'] + " in torch/onnx/symbolic.py line 232 called \n")
+
+
+    import os
+    print("TRACER WAS CALLED")
+    with open("/results/tracelog_pyt_rsub_types_corrected_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+        myfile.write(os.environ['MODEL_NAME'] + " in torch/onnx/symbolic.py line 232 called \n")
