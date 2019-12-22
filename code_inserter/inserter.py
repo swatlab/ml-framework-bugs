8f025c92f2eb282529779ab2f0d081e3909ccdf9
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-12-22 17:33:22 -0500

    Inserting works!
    still need to fix indentation and line number

--- code_inserter/inserter.py
+++ code_inserter/inserter.py
@@ -31,26 +31,62 @@ class Inserter:
 	# Old insert trace code : checkout 0450408
 
 	def insertTraces(self, filepaths, commit_number, to_be_inserted_files_lines, files_contents_lines):
+		"""
+		
+		params:
+		to_be_inserted_files_lines: same thing as lines_numbers.
+		The line number will be normal if the line can be inserted, else None
+		--> [[file_1_line_number_1, file_1_line_number_2, .. , file_1_line_number_n], 
+			[file_2_line_number_1, file_2_line_number_2, .. , file_2_line_number_p],
+			[file_m_line_number_1, file_m_line_number_2, .. , file_m_line_number_q]]
+
+		files_contents_lines: The entire text of each modified file, BUT is a 2D list
+		obtained by splitlines on each element of file_contents
+		--> [[file_1_line_1, file_1_line_2, .. , file_1_line_n], 
+			[file_2_line_1, file_2_line_2, .. , file_2_line_n],
+			[file_m_line_1, file_m_line_2, .. , file_m_line_n]]
+		"""
+		
 		# TODO find a way to use various models
 		trace_call = ['model_name = "GaussianNB"',
 		'print("TRACER WAS CALLED")',
 		'with open("/home/kacham/Documents/tracelogs/tracelog_" + commit_number + "_" + model_name + ".txt", "a") as myfile:',
 		'    myfile.write(model_name + " in  line  called \n")']
 		
-		# 
+		# Iterate through files-line in to_be_inserted_files_lines,
+		# check if line is insertable
+		# if yes, insert trace in files_contents_lines
+		traced_files_contents_lines = copy.deepcopy(files_contents_lines)
+		index=0
 		for to_be_inserted_file, file_content in zip(to_be_inserted_files_lines, files_contents_lines):
 			for to_be_inserted_line in to_be_inserted_file:
+				print(to_be_inserted_line)
 				if to_be_inserted_line != None: # that means : line can be inserted
 					# add indentation level
 					original_line = file_content[to_be_inserted_line]
 					trace_call = self.analyzerIndentation.addIndentationLevel(original_line, trace_call)
 
 					# insert trace call
-					file_content.insert(to_be_inserted_line, trace_call)
-					
+					#print(" INDEX ", index, " \n", file_content)
+					trace_index = 0
+					for trace in trace_call: 
+						traced_files_contents_lines[index].insert(to_be_inserted_line + trace_index, trace)
+						trace_index = trace_index + 1
+			index = index + 1
+
 		# Now files_contents_lines contains all inserted traces. 
-		# Write the array elements in the files
-		# for 
+		# Write each element of files_contents_lines in the files
+		for traced_file_content, filepath in zip(traced_files_contents_lines, filepaths):
+			# copy file_content array in a string
+			new_file_content = "\n".join(traced_file_content)
+			
+			# empty the file. Reference: https://stackoverflow.com/a/30604077/9876427 
+			with open(filepath, 'w'): pass
+
+			# write over it
+			with open(filepath, "w") as text_file:
+				text_file.write("{0}".format(new_file_content))
+
 		# for filepath, inserted_file in zip(filepaths, to_be_inserted_files_lines):
 		# 	file = open(filepath,"w")
 			
@@ -96,9 +132,3 @@ if __name__ == '__main__':
     
     # # test pour vérifier numéro des lignes tracées
     # inserter.printInsertedLinesNumbers(lines_numbers, traced_file_contents_lines, trace_call_Cpp)
-    
-
-# TODO
-# -portability
-# -vectorial stuff ?
-# -langage de la trace insérée
