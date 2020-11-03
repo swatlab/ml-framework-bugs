02c7f3525fab6cee906b671b0abeed389a4a3687
Emilio Rivera <emilio.rivera@polymtl.ca>
2020-11-03 14:03:17 -0500

    Update trace-inserter for better customization

--- code_inserter/trace-inserter.py
+++ code_inserter/trace-inserter.py
@@ -60,40 +60,81 @@ def cli():
 @click.option('--write/--no-write', default=True)
 @click.option('--prompt/--no-prompt', default=True)
 @click.option('--output-dir', type=str, default=True)
-@click.option('--yes', is_flag=True)
+@click.option('--yes', is_flag=True, help="Choose sensible values and no prompt")
 @click.option('--git-dir', '-d', type=click.Path(exists=True, file_okay=False, resolve_path=True))
-def diff(git_dir, pre, post, output_dir, write, prompt, yes):
-    def insert_trace(og, adds, what, use_sed=True, do_prompt=False, n_context=3):
+@click.option('--trace-content', type=str, default="""tracef('TracePoint: BugTriggered');""")
+@click.option('--trace-header', type=str, default="""#include <lttng/tracef.h>""")
+def diff(git_dir, pre, post, output_dir, write, prompt, yes, trace_content, trace_header):
+    def insert_trace(og, adds, what, do_prompt=False, n_context=3, filename=None, header=None):
         _fc = og.splitlines()
-        if use_sed:
-            sed_cmd = ['sed']
-            for l in adds:
-                if do_prompt:
-                    _l, _r = max(0, l-n_context), min(len(_fc),l+n_context)
-                    # Simple echoing
-                    # _ctx = _fc[_l:_r]
-                    # click.echo('Input here')
-                    # click.echo('\n'.join(_ctx))
-                    # ... or "fancy"
-                    click.echo('\n'.join(_fc[_l:l]))
-                    click.echo(colorama.Fore.GREEN + what +colorama.Fore.RESET)
-                    click.echo('\n'.join(_fc[l+1:_r]))
-                    if click.confirm('Add this trace in position:?'):
-                        sed_cmd.append('-e'); sed_cmd.append(f'{l}i{what}')
-                        logging.debug('Added line {} to output'.format(l))
+        def show_insertion(lines, ins_ix, content, n_context):
+            _l, _r = max(0, ins_ix-n_context), min(len(lines),ins_ix+n_context)
+            click.echo('\n'.join(lines[_l:ins_ix]))
+            click.echo(colorama.Fore.GREEN + content +colorama.Fore.RESET)
+            click.echo('\n'.join(lines[ins_ix+1:_r]))
+        
+        sed_cmd = ['sed']
+        # Ask for header insertion
+        header_insertions_line = []
+        if filename.endswith('.h') or filename.endswith('.cu') or filename.endswith('cuh') or filename.endswith('cpp'):
+            logger.info('Detected C++ file')
+            include_ix = [i for i, line in enumerate(_fc) if line.startswith('#include')]
+            if len(include_ix) != 0:
+                logger.info('Found include')
+                for k in include_ix:
+                    logger.debug('Line {}: Includes found {}'.format(k, _fc[k]))
+                if yes:
+                    # Choose last insertion point
+                    header_ins = include_ix[-1]
+                    if header_ins == 0:
+                        logger.warning('Automatic insertion of header tracing chose line 0. Will place at line 1.')
+                        header_ins = 1
                     else:
-                        logging.debug('User chose not to add to output at line {}'.format(l))
+                        logger.info('Automatic insertion of header at line {}'.format(header_ins))
+                    header_insertions_line.append(header_ins)
                 else:
-                    sed_cmd.append('-e'); sed_cmd.append(f'{l}i{what}')
-            if len(sed_cmd) == 1:
-                logging.info('No chunk to change. Returning original input.')
-                return og
-            logger.debug('Command to run')
-            logger.debug(' '.join(sed_cmd))
-            r = subprocess.run(sed_cmd, stdout=subprocess.PIPE, input=og.encode('utf-8'), check=True)
-            return r.stdout.decode("utf-8")
+                    for i, line_no in enumerate(include_ix):
+                        if line_no == 0:
+                            logger.info('Insertion at 0 unsupported')
+                            continue
+                        if do_prompt:
+                            show_insertion(_fc, line_no, header, n_context=n_context)
+                            if click.confirm('[{}/{}] Add include here?'.format(i+1, len(include_ix))):
+                                header_insertions_line.append(line_no)
+                                logger.debug('Added line {} to header output'.format(line_no))
+                            else:
+                                logger.debug('User chose not to add header')
+                        else:
+                            header_insertions_line.append(line_no)
+                            logger.debug('Added line {} to header output'.format(line_no))
         else:
-            raise NotImplementedError()
+            logger.info('File {} not supported for header insertion'.format(filename))
+
+        # Insert header
+        if len(header_insertions_line) != 0:
+            for header_lin in header_insertions_line:
+                sed_cmd.append('-e'); sed_cmd.append(f'{header_lin}i{header}')
+        else:
+            logger.info('No suitable header insertion point was found')
+
+        # Ask for trace insertion
+        for i, line_no in enumerate(adds):
+            if do_prompt:
+                show_insertion(_fc, line_no, content=what, n_context=n_context)
+                if click.confirm('[{}/{}] Add this trace in position:?'.format(i, len(adds))):
+                    sed_cmd.append('-e'); sed_cmd.append(f'{line_no}i{what}')
+                    logger.debug('Added line {} to output'.format(line_no))
+                else:
+                    logger.debug('User chose not to add to output at line {}'.format(line_no))
+            else:
+                sed_cmd.append('-e'); sed_cmd.append(f'{line_no}i{what}')
+        if len(sed_cmd) == 1:
+            logger.info('No chunk to change. Returning original input.')
+            return og
+        logger.debug('Command to run')
+        logger.debug(' '.join(sed_cmd))
+        r = subprocess.run(sed_cmd, stdout=subprocess.PIPE, input=og.encode('utf-8'), check=True)
+        return r.stdout.decode("utf-8")
 
     output_dir = Path(output_dir)
     logging.debug('Output dir is {}'.format(output_dir))
@@ -106,6 +147,7 @@ def diff(git_dir, pre, post, output_dir, write, prompt, yes):
     for f in files:
         logger.info(f)
     dp = DiffProcessor(git_dir)
+    written_files = []
     for f in files:
         logger.info('For file {}'.format(f))
         diff_for_f = diff_file(git_dir, pre, post, f)
@@ -113,10 +155,10 @@ def diff(git_dir, pre, post, output_dir, write, prompt, yes):
         # Get file content at revision
         logger.info('Lines {}'.format(' '.join(map(str,lines_changed))))
         fc = get_file_at_rev(git_dir, post, f)
-        prompt_insert = not yes or prompt
-        new_file_content = insert_trace(fc, lines_changed, '#========TRACER_CODE=========', do_prompt=prompt_insert)
+        prompt_insert = (not yes) and prompt
+        new_file_content = insert_trace(fc, lines_changed, what=trace_content, header=trace_header, do_prompt=prompt_insert, filename=f)
         logger.debug('New content')
-        logger.debug(new_file_content)
+        #logger.debug(new_file_content)
 
         if write:
             destination = output_dir.joinpath(f)
@@ -126,12 +168,13 @@ def diff(git_dir, pre, post, output_dir, write, prompt, yes):
                 with open(destination, "w") as of:
                     of.write("{0}".format(new_file_content))
                 logger.info('Wrote file to {}'.format(destination))
+                written_files.append(destination)
             # logger.debug(destination.relative_to(Path.cwd()))
             if yes:
                 w()
             else:
                 if prompt:
-                    if click.confirm('Write to {}'.format(destination)):
+                    if click.confirm('Write to {}'.format(destination), default=True):
                         w()
                     else:
                         logger.info('Cancelled write.')
@@ -139,6 +182,8 @@ def diff(git_dir, pre, post, output_dir, write, prompt, yes):
                     w()
         else:
             logging.info('Not writing output.')
+    for wf in written_files:
+        logging.info('Wrote file {}'.format(wf))
 
 if __name__ == "__main__":
     cli()
